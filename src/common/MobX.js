30393c3ba0fdf313c295bac6db4864adb50c1515
Taehyeon Ahn <antaehyeon@live.co.kr>
2020-09-02 02:01:45 +0900

    [!] package.json 버전 최신화
    - mobx 버전 업데이트로 인해 import 구문 변경 (mobx-react instead of mobx-react/native)

--- src/common/MobX.js
+++ src/common/MobX.js
@@ -3,7 +3,7 @@
 import {
   observer,
   inject,
-} from 'mobx-react/native';
+} from 'mobx-react';
 
 export function InjectedComponent(component: React$ComponentType<*>, ...stores: Array<Class<*>>) {
   return inject(...stores.map(store => store.NAME))(observer(component));
